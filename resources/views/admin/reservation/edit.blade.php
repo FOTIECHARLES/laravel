fbcbf1d39688dfb18ce291a23eed3b8edb0eb2a4
fotiecharles <sopcharles1@gmail.com>
2023-01-27 09:27:05 +0100

    Création d'une partie du CRUD

---
+++ resources/views/admin/reservation/edit.blade.php
@@ -0,0 +1,39 @@
+
+    @extends('base')
+
+    @section('page_title','Admin - Modification')
+                 
+    
+    @section('content')
+    
+    <h1>Admin - Réservation - Modification</h1>
+    <div>
+    <form action="{{ route('admin.reservation.update', ['id'=> $reservation->id])}}" method="post">
+    @csrf
+    </div>
+    <div>
+        <input type="text" name="nom" id=""value="{{ $reservation->nom }}">
+    </div>
+    <div>
+        <input type="text" name="prenom" id=""value="{{ $reservation->prenom }}">
+    </div>
+    <div>
+        <input type="date" name="jour" id=""value="{{ $reservation->jour }}">
+    </div>
+    <div>
+        <input type="time" name="heure" id=""value="{{ $reservation->heure }}">
+    </div>
+    <div>
+        <input type="number" name="nombre_personnes" id=""value="{{ $reservation->nombre_personnes }}">
+    </div>
+    <div>
+        <input type="tel" name="tel" id=""value="{{ $reservation->tel }}">
+    </div>
+    <div>
+        <input type="email" name="email" id=""value="{{ $reservation->email }}">
+    </div>
+    <div>
+     <button type="submit">Valider</button>
+    </div>   
+    </form>
+    @endsection
