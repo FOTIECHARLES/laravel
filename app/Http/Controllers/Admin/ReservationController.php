b44fd5f0220337e525cdbe621ca59091784ce382
fotiecharles <sopcharles1@gmail.com>
2023-01-27 06:57:16 +0100

    Création d'une route pour l'affichage des réservations dans le back

--- app/Http/Controllers/Admin/ReservationController.php
+++ app/Http/Controllers/Admin/ReservationController.php
@@ -1,13 +1,71 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
+use App\Models\Reservation;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class ReservationController extends Controller
 {
     public function index()
-    {
-         return view('admin.reservation.index');
+    {   
+        // récupérer la liste des reservations   
+          $reservations = Reservation::all();
+
+        // transmission des réservations à la vue
+         return view('admin.reservation.index', [
+            'reservations'=> $reservations
+        ]);
     }
+
+    public function create()
+    {
+        // valeur par défaut
+
+        //transmission des valeurs par défaut à la vue
+        return view('admin.reservation.create', [
+
+        ]);
+    }
+
+    /**
+     * Enregistre les données de la bdd
+     *
+     * @return Response
+     */
+    public function store()
+    {
+
+
+    }
+    
+
+    /**
+     * Undocumented function
+     *
+     * @param integer $id
+     * @return Response
+     */
+    public function edit(int $id)
+    {   
+        //récupération de la reservation
+        $reservation = Reservation::find($id);
+
+        // transmission de la reservation à la vue
+        return view('admin.reservation.edit',[
+            'reservation'=> $reservation,
+    
+        ]);
+    }
+    
+    /**
+     * Met à jour les données de la réservation
+     *
+     * @return Response
+     */
+    public function update(Request $request, int $id)
+    {
+        dd($request->all());
+    }
+
 }
--- resources/views/admin/reservation/index.blade.php
+++ resources/views/admin/reservation/index.blade.php
@@ -1,13 +1,45 @@
 @extends('base')
 
-@section('page_title','Admin - Réservation')
+@section('page_title','Admin - Réservation -Liste')
              
 
 @section('content')
 
-<h1>Admin - Réservation</h1>
+<h1>Admin - Réservation - Liste</h1>
+    
+    <div>
+        <a href="{{ route('admin.reservation.create')}}">Ajouter</a>
+    </div>
+
+{{-- {{dump($reservations)}} --}}
+<TABLE>
+    <TBody> 
+        <tr>
+      <th>Nom</th>
+       <th>Prenom</th>
+      <th>Jour</th>
+      <th>Heure</th>
+      <th>Nombre de Personne</th>
+      <th> tel </th>
+       <th>Email </th>
+      <th>actions</th>
 
 
+            @foreach($reservations as $reservation)
+            <TR>   
+                <TD>{{ $reservation->nom }} </TD>   
+                <TD>{{ $reservation->prenom }} </TD>
+                <TD>{{ $reservation->jour }} </TD>
+                <TD>{{ $reservation->heure }} </TD> 
+                <TD>{{ $reservation->nombre_personnes }} </TD>
+                <TD>{{ $reservation->tel  }} </TD>
+                <TD>{{ $reservation->email  }} </TD>
+                <TD><a href="{{ route( 'admin.reservation.edit', ['id' => $reservation->id])}}">modifier</a>
+                </TD>
+            </TR>
+            @endforeach
+        </TBody>
+    </TABLE>
 
 @endsection
 
