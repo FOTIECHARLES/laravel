b286a10c78e73838db998dc45c88e02a586bff19
fotiecharles <sopcharles1@gmail.com>
2023-02-20 17:14:50 +0100

    Affiche de la liste des plats sur la page plat

--- resources/views/admin/plat/index.blade.php
+++ resources/views/admin/plat/index.blade.php
@@ -4,7 +4,42 @@
              
 @section('content')
 
-    <h1>Admin - Plat - Liste</h1>
+<h1>Admin - Plat - Liste</h1>
+@if (Session::has('confirmation'))
+<div>
+    {{ Session::get('confirmation') }}
+</div>
+@endif   
+    <div>
+        <a href="{{ route('admin.plat.create')}}">Ajouter</a>
+    </div>
+
+{{-- {{dump($plats)}} --}}
+<TABLE>
+    <TBody> 
+        <tr>
+      <th>Nom</th>
+      <th>Prix</th>
+      <th>Description</th>
+      <th>Epinglé</th>
+        <tr>
+
+            @foreach($plats as $plat)
+            <TR>   
+                <TD>{{ $plat->nom  }} </TD>   
+                <TD>{{ $plat->prix  }} </TD>
+                <TD>{{ $plat->description  }} </TD>
+                <TD>{{ $plat->epingle  }} </TD> 
+                {{-- <TD><a href="{{ route( 'admin.plat.edit', ['id' => $plat->id])}}">modifier</a> --}}
+                {{-- <form action="{{ route('admin.plat.delete',['id' => $plat->id]) }}" method="post"onsubmit="return window.confirm('êtes vous sur de vouloir supprimer cet élement?');">
+                @csrf
+                @method('DELETE') --}}
+                {{-- <button type="submit">supprimer</button></form> --}}
+                {{-- </TD> --}}
+            </TR>
+            @endforeach
+        </TBody>
+    </TABLE>
 
 
 @endsection
