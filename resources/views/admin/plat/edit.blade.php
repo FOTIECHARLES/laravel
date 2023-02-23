17a89b9bf554e1f688768e7dc0c772c0fb1c7f3e
fotiecharles <sopcharles1@gmail.com>
2023-02-23 17:42:08 +0100

    Formulaire de modification du plat et de création:
    Le formulaire de modifiaction de plat dans le back office, peut maintenant enregistrer des données mais pas les envoyer à un serveur et les récuper pour qu'on les voit.
    Le formulaire de création de plat affiche maintenant les messages d'erreurs lors de la saisit des données dans l'interface graphique.

--- resources/views/admin/plat/edit.blade.php
+++ resources/views/admin/plat/edit.blade.php
@@ -19,14 +19,20 @@
     Attention, les données n'ont pas été enregistrées, il y'a des erreurs dans le formulaire.
 </div>
 @endif
-
-<div>
 <form action="{{ route('admin.plat.update', ['id'=> $plat->id])}}" method="post">
 @csrf
 @method('PUT')
+<div>
+    <label for="epingle">Epinglé</label>
+    <input class=" @error('epingle') form--input--error @enderror" type="checkbox" name="epingle" id="epingle" value="{{ old('epingle',$plat->epingle) }}">
+    @error('epingle')
+    <div class="form--error-message">
+    {{ $message }}
+    </div>
+    @enderror
 </div>
 <div>
-    <input class=" @error('nom') form--input--error @enderror"type="text" name="nom" id=""value="{{ old('nom', $plat->nom) }}">
+    <input class=" @error('nom') form--input--error @enderror" type="text" name="nom" id="" value="{{ old('nom', $plat->nom) }}">
     @error('nom')
         <div class="form--error-message">
         {{ $message }}
@@ -34,7 +40,7 @@
     @enderror
 </div>
 <div>
-    <input class=" @error('prix') form--input--error @enderror"type="number" name="prix " id=""value="{{ old('prix', $plat->prix) }}">
+    <input class=" @error('prix') form--input--error @enderror" type="number" step="any" name="prix" id="" value="{{ old('prix', $plat->prix) }}">
     @error('prix')
         <div class="form--error-message">
         {{ $message }}
@@ -42,18 +48,11 @@
     @enderror
 </div>
 <div>
-    <input class=" @error('description') form--input--error @enderror"type="text" name="description" id=""value="{{ old('description', $plat->description) }}">
+    <textarea class=" @error('description') form--input--error @enderror" name="description" id="" cols="30" rows="10" placeholder="description du plat">{{ old('description', $plat->description) }}</textarea>
     @error('description')
-    <div class="form--error-message">
-    </div>
-    @enderror
-</div>
-<div>
-    <input class=" @error('epingle') form--input--error @enderror"type="number" name="epingle" id=""value="{{ old('epingle',$plat->epingle) }}">
-    @error('epingle')
-    <div class="form--error-message">
-    {{ $message }}
-    </div>
+        <div class="form--error-message">
+        {{ $message }}
+        </div>
     @enderror
 </div>
 <div>
