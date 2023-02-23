c3e380405b063f4b3e2c13403ec21a65b7700b1c
fotiecharles <sopcharles1@gmail.com>
2023-02-23 18:02:32 +0100

    Modification de front:
    Modification de la couleur du back ground des pages du front et back office.
    Modification du nom du restaurant

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
 
 @section('content')
 
-<h1>Restaurant foo</h1>
+<h1>Bienvenue Chez Ocnamo !</h1>
     <img class="medium-size" src="{{asset('img/alex-haney-CAhjZmVk5H4-unsplash(1).jpg')}}" 
     alt="un de nos meilleurs plats">
 @endsection
