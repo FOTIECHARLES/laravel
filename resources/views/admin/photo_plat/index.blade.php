0e4158b12df2b16c40d6aef1fb5c911abe8da28b
fotiecharles <sopcharles1@gmail.com>
2023-03-05 15:52:20 +0100

    Rajout de photo dans la page liste de photo_plat

--- resources/views/admin/photo_plat/index.blade.php
+++ resources/views/admin/photo_plat/index.blade.php
@@ -15,15 +15,17 @@
     </div>
 
 {{-- {{dump($photo_plats)}} --}}
-<TABLE>
-    <TBody> 
+<table>
+    <tbody> 
         <tr>
       <th>chemin </th>
+      <th>img </th>
         <tr>
 
             @foreach($photo_plats as $photo_plat)
             <TR>   
                 <TD>{{ $photo_plat->chemin  }} </TD>   
+                <TD><img class="admin-liste-img" src="{{ asset($photo_plat->chemin)  }}" alt=""></TD>   
                 <TD><a href="{{ route( 'admin.photo_plat.edit', ['id' => $photo_plat->id])}}">modifier</a>
                 <form action="{{ route('admin.photo_plat.delete',['id' => $photo_plat->id]) }}" method="post"onsubmit="return window.confirm('êtes vous sur de vouloir supprimer cet élement?');">
                 @csrf
@@ -32,8 +34,8 @@
                 </TD>
             </TR>
             @endforeach
-        </TBody>
-    </TABLE>
+        </tbody>
+    </table>
 
 @endsection
 
