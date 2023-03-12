c3c1cfa32378384cbc072750200c8c94dfaf2661
Charles FOTIE <sopcharles1@gmail.com>
2023-03-12 20:42:23 +0100

    Modification

--- resources/views/reservation.blade.php
+++ resources/views/reservation.blade.php
@@ -17,12 +17,13 @@
         Attention, les données n'ont pas été enregistrées, il y'a des erreurs dans le formulaire.
     </div>
     @endif
-
+    <section class="reservation-section-formulaire">
     <div>
     <form action="{{ route('reservation')}}" method="post">
     @csrf
     </div>
     <div>
+        <label for="nom">Nom</label>
         <input class=" @error('nom') form--input--error @enderror"type="text" name="nom" id=""value="{{ old('nom', $reservation->nom) }}">
         @error('nom')
             <div class="form--error-message">
@@ -31,6 +32,7 @@
         @enderror
     </div>
     <div>
+        <label for="prenom">Prenom</label>
         <input class=" @error('prenom') form--input--error @enderror"type="text" name="prenom" id=""value="{{ old('prenom', $reservation->prenom) }}">
         @error('prenom')
             <div class="form--error-message">
@@ -39,6 +41,7 @@
         @enderror
     </div>
     <div>
+        <label for="jour">Jour</label>
         <input class=" @error('jour') form--input--error @enderror"type="date" name="jour" id=""value="{{ old('jour', $reservation->jour) }}">
         @error('jour')
         <div class="form--error-message">
@@ -47,6 +50,7 @@
         @enderror
     </div>
     <div>
+        <label for="heure">Heure</label>
         <input class=" @error('heure') form--input--error @enderror"type="time" name="heure" id=""value="{{ old('heure', $reservation->heure) }}" >
         @error('heure')
         <div class="form--error-message">
@@ -55,6 +59,7 @@
         @enderror
     </div>
     <div>
+        <label for="nombre_personnes">Nombre_personnes</label>
         <input class=" @error('nombre_personnes') form--input--error @enderror"type="number" name="nombre_personnes" id=""value="{{ old('nombre_personnes', $reservation->nombre_personnes) }}">
         @error('nombre_personnes')
         <div class="form--error-message">
@@ -63,6 +68,7 @@
         @enderror
     </div>
     <div>
+        <label for="tel">Téléphone</label>
         <input class=" @error('tel') form--input--error @enderror"type="tel" name="tel" id=""value="{{ old('tel', $reservation->tel) }}"placeholder="06 44 00 00 00">
         @error('tel')
         <div class="form--error-message">
@@ -71,6 +77,7 @@
         @enderror
     </div>
     <div>
+        <label for="email">Mail</label>
         <input class=" @error('email') form--input--error @enderror"type="email" name="email" id=""value="{{ old('email',$reservation->email) }}">
         @error('email')
         <div class="form--error-message">
@@ -82,6 +89,7 @@
      <button type="submit">Valider</button>
     </div>   
     </form>
+    </section>
     
 
 @endsection
