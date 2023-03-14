2c663bd3a7b84bf390a507ff717c0c639a76e6ca
Charles FOTIE <sopcharles1@gmail.com>
2023-03-14 17:31:52 +0100

    Modification de la photo de la page d'accueil

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,5 +6,5 @@
 @section('content')
 
 <h1>Bienvenue Chez Ocnamo !</h1>
-    <div class="banner" style="background-image: url('{{asset('/img/ambiance/pexels-picha-6210433.jpg')}}')"></div>
+    <div class="banner" style="background-image: url('{{asset('/img/ambiance/toa-heftiba-5JeTin55H9U-unsplash(1).jpg')}}')"></div>
 @endsection
